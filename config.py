4c91051a54b034e46ec4593d8c46db551affad9f
lukasmrohwer <lukasmrohwer@outlook.com>
2026-05-18 14:45:32 +0800

    Add ip and mac addresses to config

--- config.py
+++ config.py
@@ -1,2 +1,21 @@
 # a config.py file defining fixed parameters such as IP addresses, MAC addresses, and routing tables
 
+host_a = {
+    "ip": "10.0.1.10",
+    "mac": "AA:AA:AA:AA:AA:AA"
+}
+
+router_r1_int1 = {
+    "ip": "10.0.1.1",
+    "mac": "BB:BB:BB:BB:BB:BB"
+}
+
+router_r1_int2 = {
+    "ip": "10.0.2.1",
+    "mac": "CC:CC:CC:CC:CC:CC"
+}
+
+host_b = {
+    "ip": "10.0.2.20",
+    "mac": "DD:DD:DD:DD:DD:DD"
+}
