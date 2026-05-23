b01fc273a2db6a876d15699ffd12526bf26dff50
lukasmrohwer <lukasmrohwer@outlook.com>
2026-05-23 20:37:52 +0800

    Comment devices.py

--- devices.py
+++ devices.py
@@ -4,6 +4,7 @@ from config import *
 
 class Host:
     def __init__(self, name, ip, mac, routing_table, arp_table):
+        """This function intialises the host based of the parameters it is given from main.py"""
         self.name = name
         self.ip = ip
         self.mac = mac
@@ -15,18 +16,22 @@ class Host:
         self.router = None    
         self.macTable = {}
 
+        # creates protocol layers
         self.TransportLayer = TransportLayer(self)
         self.NetworkLayer = NetworkLayer(self)
         self.LinkLayer = LinkLayer(self)
 
     def application_send(self, data, src_port, dst_port, dst_ip):
+        """This function simulates the application layer handing off data to the transport layer to initiate the chain."""
         self.TransportLayer.send(data, src_port, dst_port, dst_ip)
 
     def link_receive(self, frame):
+        """This function simulates the host receiving data from the network."""
         self.LinkLayer.receive(frame)
 
 class Router:
     def __init__(self, name, routing_table, arp_table):
+        """This function intialises the router based of the parameters it is given from main.py"""
         self.name = name
         self.routingTable = routing_table       
         self.arpTable = arp_table               
@@ -34,13 +39,16 @@ class Router:
         self.hostA = None                       
         self.hostB = None                       
 
+        # defines the two interfaces on the router
         self.interfaces = {
             "Interface 1": {"ip": ROUTER_R1_INT1_IP, "mac": ROUTER_R1_INT1_MAC},
             "Interface 2": {"ip": ROUTER_R1_INT2_IP, "mac": ROUTER_R1_INT2_MAC},
         }
 
+        # creates protocol layers
         self.NetworkLayer = NetworkLayer(self)
         self.LinkLayer = LinkLayer(self)
 
     def link_receive(self, frame, interface):
+        """This function simulates the router receiving data from the network."""
         self.LinkLayer.receive(frame, interface)
